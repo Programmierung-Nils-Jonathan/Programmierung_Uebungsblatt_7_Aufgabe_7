43856e6c6ec7f59fc17c0a51ae5722c5623d7fc3
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2021-12-08 13:56:48 +0000

    added initial files

---
+++ Command.java
@@ -0,0 +1,11 @@
+public abstract class Command {
+
+    public void execute() {
+        // override me!
+    }
+
+    public static Command parse(String cmdName, VCS vcs) {
+        // implement me!
+        return null;
+    }
+}
