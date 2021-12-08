d64813a5471357fab701b65ec247bb39edb0283f
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2021-12-08 15:35:49 +0000

    implemented subtask d

---
+++ Commit.java
@@ -0,0 +1,47 @@
+public class Commit extends ListFiles {
+
+    public Commit(VCS vcs) {
+        super(vcs);
+    }
+
+    @Override
+    public void execute() {
+        String timeStampDir = this.createTimeStampDir();
+
+        this.moveOldBackupDirFiles(timeStampDir);
+
+        this.copyRootFiles();
+
+        System.out.println("Committed the following files:");
+
+        super.execute();
+    }
+
+    private String createTimeStampDir() {
+        String timeStampDir = Util.appendFileOrDirname(vcs.getBackupDir(), Util.getTimestamp());
+        Util.mkdir(timeStampDir);
+
+        return timeStampDir;
+    }
+
+    private void moveOldBackupDirFiles(String timeStampDir) {
+        String[] backupFiles = Util.listFiles(vcs.getBackupDir());
+
+        for(String file : backupFiles) {
+            String srcFilename = Util.appendFileOrDirname(vcs.getBackupDir(), file);
+            String destFilename = Util.appendFileOrDirname(Util.appendFileOrDirname(vcs.getBackupDir(), timeStampDir), file);
+            Util.moveFile(srcFilename, destFilename);
+        }
+    }
+
+    private void copyRootFiles() {
+        String[] rootFiles = Util.listFiles(vcs.getRootDir());
+
+        for(String file : rootFiles) {
+            String srcFilename = Util.appendFileOrDirname(vcs.getRootDir(), file);
+            String destFilename = Util.appendFileOrDirname(vcs.getBackupDir(), file);
+            Util.copyFile(srcFilename, destFilename);
+        }
+    }
+    
+}
